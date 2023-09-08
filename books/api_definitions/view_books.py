2344902f006c3408e30b247a7e55f2bd36c3e409
himabindubontha333 <bonthahimabindu123@gmail.com>
2023-09-08 21:39:16 +0530

    first commit

--- books/api_definitions/view_books.py
+++ books/api_definitions/view_books.py
@@ -1,18 +1,10 @@
 from books.api import books_api
 from books.schema_definitions.create_book_schema import ViewBookSchema
 from books.config import VIEW_BOOKS_API
-from books.models import *
-from rest_framework.decorators import api_view, permission_classes
-from books.permissions import *
+from rest_framework import permissions
 
-from rest_framework.permissions import IsAuthenticated
-
-
-@permission_classes([IsAuthenticated,IsOwnerOrReadOnly,AdminPermission])
-@books_api.get(VIEW_BOOKS_API,data=ViewBookSchema)
-def VIEW_BOOKS_API(request, data=ViewBookSchema):
-       try:
-         user_Model = UserProfile.objects.get(user=request.user)
-         return user_Model.can_view_books
-       except UserProfile.DoesNotExist:
-            return False
+class CanDeleteBook(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (
+            request.user.Role == 'Moderator' or request.user.Role == 'Admin' or request.user.Role=='Moderator'
+        )  
